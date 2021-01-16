e399523b215a8f6635272fe59e3cb922b2bdb67f
K.Sachith Malshan <55049681+ksmalshan@users.noreply.github.com>
2021-01-16 14:00:58 +0530

    Create user.dart

---
+++ lib/models/user.dart
@@ -0,0 +1,19 @@
+class User {
+
+  final String uid;
+  
+  User({ this.uid });
+
+}
+
+
+class UserData {
+ final String uid;
+  final String arivalTime;
+  final String departureTime;
+  final String busName;
+  final String telephone;
+
+  UserData({ this.uid, this.arivalTime, this.departureTime, this.busName, this.telephone});
+
+}
